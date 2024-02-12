eb5ba93fc367bc352885c363a91e98880a56b9b8
user530 <m.timarsuev@gmail.com>
2024-02-12 23:04:20 +0300

    Form item updated to handle validation

--- src/features/form/form-item/Form-item.tsx
+++ src/features/form/form-item/Form-item.tsx
@@ -5,12 +5,15 @@ interface IFormItem {
     itemLabel: string;
     itemPlaceholder?: string;
     itemType?: 'text' | 'password';
-    isError?: boolean;
+    errHandling?: {
+        validationCb: (value: string) => boolean;
+        validationErr: string;
+    }
 };
 
 export const FormItem: React.FC<IFormItem> = (props: IFormItem) => {
-    const { itemLabel, itemType, itemPlaceholder, isError } = props;
-    const [error, setError] = React.useState<boolean>(isError ? isError : false);
+    const { itemLabel, itemType, itemPlaceholder, errHandling } = props;
+    const [error, setError] = React.useState<boolean>(false);
 
     return (
         <div className={styles['form-item']}>
@@ -22,11 +25,22 @@ export const FormItem: React.FC<IFormItem> = (props: IFormItem) => {
                 type={itemType ? itemType : 'text'}
                 className={`${styles['form-item__input']}${error ? ' ' + styles['form-item__input--error'] : ''}`}
                 placeholder={itemLabel ? itemPlaceholder : ''}
+                onBlur={
+                    errHandling
+                        ? (e) => {
+                            const isValid = errHandling?.validationCb(e.target.value);
+                            setError(!isValid);
+                        }
+                        : () => { }
+                }
             />
 
-            {error
-                ? <span className={styles['form-item__error']}>Ошибка</span>
-                : null
+            {
+                error
+                    ? <span className={styles['form-item__error']}>
+                        {errHandling?.validationErr ? errHandling.validationErr : ''}
+                    </span>
+                    : null
             }
         </div>
     )
