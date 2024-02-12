5e63b2a57003c8553f6dc96d77a63f6ac8d51945
user530 <m.timarsuev@gmail.com>
2024-02-12 23:04:33 +0300

    Form component updated

--- src/features/form/Form.tsx
+++ src/features/form/Form.tsx
@@ -4,10 +4,11 @@ import { FormItem } from './form-item/Form-item';
 
 interface IFormComponent {
     formName: string;
+    btnText: string;
 }
 
 export const Form: React.FC<IFormComponent> = (props: IFormComponent) => {
-    const { formName } = props;
+    const { formName, btnText } = props;
 
     return (
         <form className={styles['form__wrapper']}>
@@ -18,12 +19,16 @@ export const Form: React.FC<IFormComponent> = (props: IFormComponent) => {
             <div className={styles['form__body']}>
                 <FormItem itemLabel='Имя' itemPlaceholder='Ваше имя' />
                 <FormItem itemLabel='Электронная почта' itemPlaceholder='Ваш e-mail' />
-                <FormItem itemLabel='Пароль' itemType='password' isError={true} />
+                <FormItem itemLabel='Пароль' itemType='password'
+                    errHandling={{
+                        validationCb: (val) => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/.test(val),
+                        validationErr: 'Пароль должен быть не меньше 6 символов и включать одну цифру, одну заглавную и малую буквы, а также один спец символ!'
+                    }} />
                 <FormItem itemLabel='Подтвердите пароль' itemType='password' />
             </div>
 
             <div className={styles['form__footer']}>
-                <input type="submit" value="" />
+                <input className={styles['button']} type="submit" value={btnText} />
             </div>
         </form>
     );
