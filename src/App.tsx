0fed98d306dc66f3ea52483ff7d795df3913f6bb
user530 <m.timarsuev@gmail.com>
2024-02-13 22:17:16 +0300

    Added a route for the Catalogue page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Signup } from './pages/Signup/Signup';
+import { Catalogue } from './pages/Catalogue/Catagolue';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Signup />} />
+        <Route path='/catalogue' element={<Catalogue />} />
       </Routes>
     </BrowserRouter>
   );
