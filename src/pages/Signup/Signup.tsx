173adbde99b4d12218cca5f8104cd4f0498d1d99
user530 <m.timarsuev@gmail.com>
2024-02-13 22:18:02 +0300

    Signup page updated to handle dynamic registration form

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -2,12 +2,60 @@ import React from 'react';
 import styles from './Signup.module.scss';
 import { Form } from '../../features/form/Form';
 
+interface FormItemData {
+    itemName: string;
+    itemType: 'text' | 'password';
+    itemLabel: string;
+    itemPlaceholder?: string;
+    itemValidation?: {
+        validationCb: (value: string) => boolean;
+        validationErr: string;
+    }
+}
+
 export const Signup: React.FC = () => {
+    const registerFormFields: FormItemData[] = [
+        {
+            itemName: 'name',
+            itemType: 'text',
+            itemLabel: 'Имя',
+            itemPlaceholder: 'Ваше имя',
+        },
+        {
+            itemName: 'email',
+            itemType: 'text',
+            itemLabel: 'Электронная почта',
+            itemPlaceholder: 'Ваш e-mail',
+        },
+        {
+            itemName: 'password',
+            itemType: 'password',
+            itemLabel: 'Пароль',
+            itemValidation:
+            {
+                validationCb: (val) => /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/.test(val),
+                validationErr: 'Пароль должен быть не меньше 6 символов и включать одну цифру, одну заглавную и малую буквы, а также один спец символ!'
+            }
+        },
+        {
+            itemName: 'password2',
+            itemType: 'password',
+            itemLabel: 'Подтвердите пароль',
+        },
+    ]
 
     return (
         <div className={styles['wrapper']}>
-            <Form formName='Регистрация' btnText='Зарегистрироваться' />
-            {/* <Form formName='Авторизация' btnText='Войти' /> */}
+            <Form
+                formName='Регистрация'
+                btnText='Зарегистрироваться'
+                formFields={registerFormFields}
+                submitHandler={(e) => {
+                    console.log('SUBMIT FORM FIRED!');
+                    const form = e.target as HTMLFormElement;
+                    const data = new FormData(form);
+                }} />
+
         </div>
     )
 }
