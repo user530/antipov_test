ee7fcccb8d34b7e5e890849f8de9f5708873bbe0
user530 <m.timarsuev@gmail.com>
2024-02-13 23:39:11 +0300

    Person component initial implementation

--- src/pages/Person/Person.tsx
+++ src/pages/Person/Person.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import styles from './Person.module.scss';
+import { BsTelephone, BsEnvelope } from "react-icons/bs";
 
 export const Person: React.FC = () => {
     const personData = {
         imgSrc: 'https://placehold.co/124x124',
         name: 'Артур Королёв',
         role: 'Партнёр',
-        phone: '+7 (954) 333-44-55',
-        email: 'sykfafkar@gmail.com',
+        contacts: [
+            {
+                icon: <BsTelephone />,
+                href: 'tel:',
+                value: '+7 (954) 333-44-55'
+            },
+            {
+                icon: <BsEnvelope />,
+                href: 'mailto:',
+                value: 'sykfafkar@gmail.com'
+            },
+        ],
         bio: `Клиенты видят в нем эксперта по вопросам разработки комплексных решений финансовых продуктов, включая такие аспекты, как организационная структура, процессы, аналитика и ИТ-компоненты. Он помогает клиентам лучше понимать структуру рисков их бизнеса, улучшать процессы за счет применения новейших технологий и увеличивать продажи, используя самые современные аналитические инструменты.
         В работе с клиентами недостаточно просто решить конкретную проблему или помочь справиться с трудностями. Не менее важно уделять внимание обмену знаниями: "Один из самых позитивных моментов — это осознание того, что ты помог клиенту перейти на совершенно новый уровень компетентности, уверенность в том, что после окончания проекта у клиента есть все необходимое, чтобы дальше развиваться самостоятельно".
         Помимо разнообразных проектов для клиентов финансового сектора, Сорин ведет активную предпринимательскую деятельность. Он является совладельцем сети клиник эстетической медицины в Швейцарии, предлагающей инновационный подход к красоте, а также инвестором других бизнес-проектов.`,
@@ -22,30 +33,47 @@ export const Person: React.FC = () => {
                 </div>
                 <div className={styles['header-content']}>
 
-                    <div className="content-container">
-                        <div className="container-col">
-                            <div className="header-img">
+                    <div className={styles['container']}>
+                        <div className={styles['inner-header']}>
+                            <div className={styles['header-img']}>
                                 <img src={personData.imgSrc} alt={personData.name} width={187} height={187} />
                             </div>
-                        </div>
-                        <div className="container-col">
-                            <div className="header-info">
-                                <h1 className="info-main">{personData.name}</h1>
-                                <h2 className="sub">{personData.role}</h2>
+                            <div className={styles['header-info']}>
+                                <h1 className={styles['info-main']}>{personData.name}</h1>
+                                <h2 className={styles['info-sub']}>{personData.role}</h2>
                             </div>
                         </div>
                     </div>
-
-
-
-
-
                 </div>
             </div>
 
             <div className={styles['catalogue-body']}>
-                <div className={styles['body-container']}>
-                    CONTENT
+                <div className={styles['container']}>
+                    <div className={styles['inner-body']}>
+                        <div className={styles['inner-body__bio']}>
+                            {
+                                personData.bio.split('\n').map(
+                                    (txtBlock, ind) => (<p key={`txt-${ind}`}>{txtBlock}</p>)
+                                )
+                            }
+                        </div>
+                        <div className={styles['inner-body__contacts']}>
+                            <ul className={styles['contacts-list']}>
+                                {
+                                    personData.contacts.map(
+                                        ({ icon, href, value }, ind) => (
+                                            <li key={`contact-${ind}`}>
+                                                <a href={href + value}>
+                                                    <span className={styles['contact-icon']}>{icon}</span>
+                                                    <span>{value}</span>
+                                                </a>
+                                            </li>
+                                        )
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
