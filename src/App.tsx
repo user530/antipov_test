61c56905e8efe58c25efe6fe79fbc30c04bc3fb2
user530 <m.timarsuev@gmail.com>
2024-02-13 23:40:01 +0300

    Router added single person page + small style fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Signup } from './pages/Signup/Signup';
 import { Catalogue } from './pages/Catalogue/Catagolue';
+import { Person } from './pages/Person/Person';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Signup />} />
         <Route path='/catalogue' element={<Catalogue />} />
+        <Route path='/person' element={<Person />} />
       </Routes>
     </BrowserRouter>
   );
