c714d9cc1ba19260c45ea9d57c507c831998045a
user530 <m.timarsuev@gmail.com>
2024-03-23 18:09:37 +0300

    Refactored form item to handle generic and password inputs differently

--- src/features/form/form-item/Form-item.tsx
+++ src/features/form/form-item/Form-item.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Form-item.module.scss';
 import { useFormContext } from '../Form.context';
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 export interface FormItemProps {
     itemName: string;
@@ -11,7 +12,7 @@ export interface FormItemProps {
 };
 
 export const FormItem: React.FC<FormItemProps> = (props: FormItemProps) => {
-    const { itemName, itemLabel, itemPlaceholder, itemType, itemValidation } = props;
+    const { itemName, itemLabel, itemType, itemPlaceholder, itemValidation } = props;
     const { formValues, formErrors, handleChange, setFieldError, clearFieldError } = useFormContext();
 
     const itemValue = formValues[itemName];
@@ -32,27 +33,82 @@ export const FormItem: React.FC<FormItemProps> = (props: FormItemProps) => {
             setFieldError(itemName, validationError);
     };
 
+    const formItemProps = {
+        itemName,
+        itemPlaceholder,
+        itemValue,
+        itemError,
+        changeHandler,
+        blurHandler,
+    }
+
     return (
         <div className={styles['form-item']}>
             <label className={styles['form-item__label']}>
                 {itemLabel}
             </label>
 
-            <input
-                type={itemType ? itemType : 'text'}
-                className={`${styles['form-item__input']}${itemError ? ' ' + styles['form-item__input--error'] : ''}`}
-                name={itemName}
-                value={itemValue}
-                placeholder={itemPlaceholder ? itemPlaceholder : ''}
-                onChange={changeHandler}
-                onBlur={blurHandler}
-            />
+            {
+                itemType === 'password'
+                    ? <PasswordFormItem {...formItemProps} />
+                    : <GenericFormItem {...formItemProps} itemType={itemType} />
+            }
 
             {
                 itemError
                     ? <span className={styles['form-item__error']}>{itemError}</span>
                     : null
             }
+        </div>
+    )
+}
+
+interface FormInputProps extends Omit<FormItemProps, 'itemLabel' | 'itemValidation'> {
+    itemValue: string;
+    itemError: string;
+    changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    blurHandler: () => void;
+}
+
+const GenericFormItem: React.FC<FormInputProps> = (props) => {
+    const { itemName, itemType, itemPlaceholder, itemValue, itemError, changeHandler, blurHandler } = props;
+
+    return (
+        <input
+            type={itemType ? itemType : 'text'}
+            className={`${styles['form-item__input']}${itemError ? ' ' + styles['form-item__input--error'] : ''}`}
+            name={itemName}
+            value={itemValue}
+            placeholder={itemPlaceholder ? itemPlaceholder : ''}
+            onChange={changeHandler}
+            onBlur={blurHandler}
+        />
+    )
+}
+
+interface PasswordInputProps extends Omit<FormInputProps, 'itemType'> { }
+
+const PasswordFormItem: React.FC<PasswordInputProps> = (props) => {
+    const [isVisible, setIsVisible] = React.useState<boolean>(false);
+
+    const iconClickHandler = () => {
+        setIsVisible(current => !current);
+    };
+
+    return (
+        <div className={styles['form-item__input-wrapper']}>
+            <GenericFormItem
+                itemType={isVisible ? 'text' : 'password'}
+                {...props}
+            />
+
+            <button className={styles['form-item__visibility']} onClick={iconClickHandler} >
+                {
+                    isVisible
+                        ? <FiEye />
+                        : <FiEyeOff />
+                }
+            </button>
         </div>
     )
 }
